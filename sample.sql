5db5dac0a595cb8c2235e1911f63a3b92233233f
ShiroNeko <74762641+ItsShiroNeko@users.noreply.github.com>
2024-07-17 11:11:15 +0700

    change sample.sql

--- sample.sql
+++ sample.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 06 Jun 2024 pada 21.45
--- Versi server: 10.4.28-MariaDB
--- Versi PHP: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `manage_db`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `item`
---
 
 CREATE TABLE `item` (
   `id` int(11) NOT NULL,
@@ -35,54 +10,25 @@ CREATE TABLE `item` (
   `total` int(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data untuk tabel `item`
---
 
 INSERT INTO `item` (`id`, `item`, `stock`, `used`, `total`) VALUES
 (5, 'Chain Sling', '12', '5', 17),
 (6, 'galvanized square steel', '5', '20', 25),
 (7, 'Eco Friendly Wood', '20', '5', 25);
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user`
---
 
 CREATE TABLE `user` (
   `username` varchar(10) NOT NULL,
   `password` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data untuk tabel `user`
---
 
 INSERT INTO `user` (`username`, `password`) VALUES
 ('admin', 'admin');
 
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `item`
---
 ALTER TABLE `item`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `item`
---
 ALTER TABLE `item`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
